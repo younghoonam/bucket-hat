dbd732a5b4fdf65c2989dc68ec1ee8556a9db013
Younghoo Nam <nyounghoo@gmail.com>
2025-05-13 23:42:28 +0900

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -47,21 +47,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics Script */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.MEASUREMENT_ID}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.MEASUREMENT_ID}');
-            `,
-          }}
-        />
+        {/* GTM */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
